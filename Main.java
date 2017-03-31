fcb304a83384933f1143f1f8920d06dbdb3b8c90
gellyviana <gellyviana@outlook.com>
2017-03-30 22:53:21 -0300

    alteração no Main

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ContaBancaria cliente1 = new ContaCorrente("Gelly", 2555, 417036, 50.0);
-		ContaBancaria cliente2 = new ContaPoupanca("Marciel", 4587, 555555, 190.0);
+		ContaBancaria cliente2 = new ContaPoupanca("Marciel", 4587, 555555, 90.0);
 		System.out.println("========== 1 ==========");
 		cliente1.efetuarDeposito(500.0);
 		cliente1.imprimeConta(cliente1);
